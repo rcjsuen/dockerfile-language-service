c2b5d0cea713eea24f528d3c50e0a1211c62c38a
Remy Suen <remy.suen@gmail.com>
2019-10-13 16:25:07 -0400

    Ensure tag completion code is not getting zero results
    Signed-off-by: Remy Suen <remy.suen@gmail.com>

--- test/dockerAssist.registry.test.ts
+++ test/dockerAssist.registry.test.ts
@@ -30,6 +30,8 @@ function assertImageTag(tag: string, item: CompletionItem, line: number, charact
 }
 
 function assertImageTags(tags: string[], items: CompletionItem[], cursorPosition: number, prefixLength: number) {
+    assert.notEqual(items.length, 0);
+    assert.notEqual(tags.length, 0);
     assert.equal(items.length, tags.length);
     for (let i = 0; i < tags.length; i++) {
         assertImageTag(tags[i], items[i], 0, cursorPosition - prefixLength, prefixLength);
