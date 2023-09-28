0254e406cfdc36b801e864b283e77d252333cafc
Seth Pellegrino <seth@codecopse.net>
2023-09-28 08:57:05 -0700

    fix(nodejs>16): upgrade webpack for example
    Prior to this change, trying to build with more recent versions of node
    attempts to make use of insecure (& now-removed) hash functions.
    
    See: https://stackoverflow.com/a/73027407

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -3,12 +3,14 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
 const path = require('path');
+const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const buildRoot = path.resolve(__dirname, "lib");
 const monacoEditorPath = './node_modules/monaco-editor/min/vs';
 
 module.exports = {
+    mode: "production", // see https://webpack.js.org/configuration/mode/
     entry: path.resolve(buildRoot, "main.js"),
     output: {
         filename: 'bundle.js',
@@ -45,6 +47,10 @@ module.exports = {
                     to: 'vs'
                 }
             ]
-        })
+        }),
+        // https://stackoverflow.com/a/71515171
+        new webpack.ProvidePlugin({
+            Buffer: ['buffer', 'Buffer'],
+        }),
     ]
 }
